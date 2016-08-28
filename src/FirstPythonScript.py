57b49b15293ebee13e155c409ae64619c50aca62
harshVRastogi <harsh@droidharsh.com>
2016-08-28 00:22:59 -0700

    Little information on comments

--- src/FirstPythonScript.py
+++ src/FirstPythonScript.py
@@ -1,4 +1,8 @@
+# A hash is used for commenting a line.
+# Keep the hash in the start of a line.
+# Multi line comments are achieved by just putting a hash in the starting of every line
 print "Hello World!"
 print "PyCharms works Charming"
 print 'Started using Python'
+# Double quotes "" and '' single quotes both work in python for string literal
 print 'Python is Awesome'
