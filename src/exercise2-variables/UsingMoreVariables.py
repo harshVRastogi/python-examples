124b3ab6fea58dab48e187961c322272353404b6
harshVRastogi <harsh@droidharsh.com>
2016-08-28 01:41:53 -0700

    Working more with variables

---
+++ src/exercise2-variables/UsingMoreVariables.py
@@ -0,0 +1,15 @@
+my_name = "Harsh Rastogi"
+my_age = 23
+my_height = 175  # cms
+my_weight = 62  # kgs
+my_eyes = "brown"
+my_teeth = "yellow"
+my_hair = "black"
+
+# %s is one of the format specifiers which is used to modify a string.
+print "Let's talk about %s ." % my_name
+print "He is %d centimeters tall." % my_height
+print "He is %d kilograms heavy." % my_weight
+print "Working on gaining weight."
+print "He has got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are always %s whatever happens." % my_teeth
