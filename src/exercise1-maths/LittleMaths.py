0dc7a670ea70368736fe02dab09cbd1f9cc2ab4a
harshVRastogi <harsh@droidharsh.com>
2016-08-28 01:38:11 -0700

    Little more in maths

---
+++ src/exercise1-maths/LittleMaths.py
@@ -0,0 +1,45 @@
+print "How much money I had in the beginning ? Rs.", 10000
+
+print "How much should it become after adding 1000? Rs.", 10000 + 1000
+
+print "Then I gave away 5000. Remaining : Rs.", 11000 - 5000
+
+print "Again gave away 4000. Remaining : Rs.", 6000 - 4000
+
+print "The Other guy had Rs.", 3000
+
+print "Does he have more than I have?"
+
+print 3000 > 2000
+
+print "Does he have less than I have?"
+
+print 3000 < 2000
+
+print "what's the difference? "
+
+print 3000 - 2000
+
+print "what's the ratio of 20 and 10? "
+
+print 20 / 10
+
+# Dividing decimal numbers
+print "Divide 5.7 by 2.3 ", 5.7 / 2.3
+
+# BODMAS is followed in the python for order also.
+print "Calculate with some random numbers -> ", 32 + 27 - 81 / 9 * 1
+
+print "Is -99999 greater than 1? ", -99999 > 1
+
+print "Is -99999 smaller than 1? ", -99999 < 1
+print "Are -99999 and 1 equal? ", -99999 == 1
+
+print "Is 4 is greater than or  equal to 4? ", 4 >= 4
+print "Is 5 is smaller than or equal to 6? ", 5 <= 6
+
+# The operator % does not work as a percentage calculator. It just give the remainder.
+
+print "What is the remainder of 5 divide by 2? ", 5 % 2
+# Operator % gives the whole value.
+print "What is the remainder of 1000 divide by 3? ", 1000 / 3
